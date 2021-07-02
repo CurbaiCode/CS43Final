b36af5fa31de84080f29472417dc5d64d1640a3c
Fred-Doug <47488521+Fred-Doug@users.noreply.github.com>
2021-07-01 20:35:27 -0600

    Update oop_tictactoe.py to first usable version

--- oop_tictactoe.py
+++ oop_tictactoe.py
@@ -1,61 +1,134 @@
 import random
 
-sym = ["X", "O"]
-names = ["", ""]
-symbols = ["", ""]
+symbols = ["X", "O"]
 
 class Game:
 
-    def __init__(self, name1, symbol1, name2, symbol2):
+    def __init__(self):
         self.board = Board()
-        self.p1 = Player(name1, symbol1)
-        self.p2 = Player(name2, symbol2)
+        self.p1 = Player(0)
+        self.p2 = Player(1)
+        if random.choice(range(2)) == 0:
+            self.p1.setSymbol()
+            self.p2.sym = symbols[int(not bool(symbols.index(self.p1.sym)))]
+            self.cur = self.p2
+            self.next = self.p1
+        else:
+            self.p2.setSymbol()
+            self.p1.sym = symbols[int(not bool(symbols.index(self.p2.sym)))]
+            self.cur = self.p1
+            self.next = self.p2
 
-    def display_board(self):
+    def displayBoard(self):
         self.board.display()
+
+    def instructions(self):
+        print("How to Play")
+
+    def makeMove(self, player, square):
+        while self.board.update(player.sym, square) is None:
+            square = input("Sorry {}, you can't move there. Try again: ".format(player.name))
+
+        return self.board.update(player.sym, square)
+
+    def isWon(self, player):
+        return self.board.isWinner(player)
+
+    def isDraw(self):
+        return self.board.noMoves()
+
+    def switchTurns(self):
+        self.cur, self.next = self.next, self.cur
 
 
 class Board:
 
     def __init__(self):
-        self.square = {
+        self.squares = {
             "7": " ", "8": " ", "9": " ",
             "4": " ", "5": " ", "6": " ",
             "1": " ", "2": " ", "3": " "
         }
 
     def display(self):
-        print(self.square["7"] + "|" + self.square["8"] + "|" + self.square["9"])
+        print()
+        print(self.squares["7"] + "|" + self.squares["8"] + "|" + self.squares["9"])
         print("-+-+-")
-        print(self.square["4"] + "|" + self.square["5"] + "|" + self.square["6"])
+        print(self.squares["4"] + "|" + self.squares["5"] + "|" + self.squares["6"])
         print("-+-+-")
-        print(self.square["1"] + "|" + self.square["2"] + "|" + self.square["3"])
+        print(self.squares["1"] + "|" + self.squares["2"] + "|" + self.squares["3"])
 
-    def instructions(self):
-        print("How to Play")
+    def squareEmpty(self, square):
+        if square in self.squares and self.squares[square] == " ":
+            return True
+
+        return False
+
+    def update(self, symbol, square):
+        if self.squareEmpty(square):
+            self.squares[square] = symbol
+            return self.squares
+
+        return None
+
+    def isWinner(self, player):
+        possibleWins = [
+            ["7", "8", "9"],
+            ["4", "5", "6"],
+            ["1", "2", "3"],
+            ["7", "4", "1"],
+            ["8", "5", "2"],
+            ["9", "6", "3"],
+            ["7", "5", "3"],
+            ["1", "5", "9"]
+        ]
+        for x, y, z in possibleWins:
+            if self.squares[x] == self.squares[y] == self.squares[z] == player.sym:
+                return True
+
+        return False
+
+    def noMoves(self):
+        for square in self.squares:
+            if self.squares[square] == " ":
+                return False
+
+        return True
 
 
 class Player:
 
-    def __init__(self, name, symbol):
-        self.name = name
-        self.symbol = symbol
+    def __init__(self, number):
+        self.name = input("What's player {}'s name? ".format(str(number + 1)))
+        self.sym = ""
         self.score = 0
 
+    def validSymbol(self, symbol):
+        if symbol in symbols:
+            return True
 
-names[0] = input("What's player 1's name? ")
-names[1] = input("What's player 2's name? ")
-if random.choice(range(2)) == 0:
-    symbols[0] = input("{}, pick a symbol (X/O): ".format(names[0])).upper()
-    symbols[1] = sym[int(not bool(sym.index(symbols[0])))]
-    currentP = 1
-    nextP = 0
-else:
-    symbols[1] = input("{}, pick a symbol (X/O): ".format(names[1])).upper()
-    symbols[0] = sym[int(not bool(sym.index(symbols[1])))]
-    currentP = 0
-    nextP = 1
+        return False
 
-g = Game(names[0], symbols[0], names[1], symbols[1])
-g.display_board()
-move = input("{}, what's your move? ".format(names[currentP]))
+    def setSymbol(self):
+        symbol = input("{}, pick a symbol ({}/{}): ".format(self.name, symbols[0], symbols[1])).upper()
+        while self.validSymbol(symbol) is False:
+            symbol = input("That's not a valid symbol {}, pick \"{}\" or \"{}\": ".format(self.name, symbols[0], symbols[1])).upper()
+
+        self.sym = symbol
+
+
+g = Game()
+while True:
+    g.displayBoard()
+    move = input("{}, what's your move? ".format(g.cur.name))
+    g.makeMove(g.cur, move)
+    if g.isWon(g.cur):
+        g.displayBoard()
+        print("{} wins!".format(g.cur.name))
+        break
+    elif g.isDraw():
+        g.displayBoard()
+        print("Draw!")
+        break
+
+    g.switchTurns()
